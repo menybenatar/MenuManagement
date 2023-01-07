5c235d6c94d42cd380d0da8ef34ff5e7962c8d78
Meny Ben Atar <menybenatar@gmail.com>
2023-01-07 12:56:53 +0200

    Create event handler method

--- Ex04.Menus.Test/DelegateTestMenu.cs
+++ Ex04.Menus.Test/DelegateTestMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Ex04.Menus.Delegates;
@@ -13,15 +14,50 @@ namespace Ex04.Menus.Test
         {
             MainMenu mainMenu = new MainMenu();
             SubMenu firstSub = new SubMenu("Version and Uppercase");
-            firstSub.AddMenuItem(new ActionItem("Show Version", eActionType.Event));
-            firstSub.AddMenuItem(new ActionItem("Count Uppercase", eActionType.Event));
-            SubMenu secondSub = new SubMenu("Show Date/Time");
-            secondSub.AddMenuItem(new ActionItem("Show Date", eActionType.Event));
-            secondSub.AddMenuItem(new ActionItem("Show Time", eActionType.Event));
-            secondSub.AddMenuItem(new SubMenu("Submenu"));
+            ActionItem versionAction = new ActionItem("Show Version", eActionType.Event);
+            ActionItem countUppercaseAction = new ActionItem("Count Uppercase", eActionType.Event);
+            versionAction.Selected += versionAction_Selected;
+            countUppercaseAction.Selected += countUppercaseAction_Selected;
+            firstSub.AddMenuItem(versionAction);
+            firstSub.AddMenuItem(countUppercaseAction);
             mainMenu.MainMenuItems.AddMenuItem(firstSub);
+
+            SubMenu secondSub = new SubMenu("Show Date/Time");
+            ActionItem timeAction = new ActionItem("Show Time", eActionType.Event);
+            ActionItem dateAction = new ActionItem("Show Date", eActionType.Event);
+            dateAction.Selected += dateAction_Selected;
+            timeAction.Selected += timeAction_Selected;
+            secondSub.AddMenuItem(dateAction);
+            secondSub.AddMenuItem(timeAction);
             mainMenu.MainMenuItems.AddMenuItem(secondSub);
+
             mainMenu.Show();
+        }
+
+        private static void countUppercaseAction_Selected()
+        {
+            int uppercaseCounter = 0;
+            Console.WriteLine($"Please Write A Sentence.{Environment.NewLine}The Program Will Count Uppercase Letters In Your Sentence.");
+            string userSentence = Console.ReadLine();
+            uppercaseCounter = userSentence.ToCharArray().Count(c => char.IsUpper(c));
+            Console.WriteLine($"The Number Of Uppercase In Your Sentence Is: {uppercaseCounter}.");
+        }
+
+        private static void versionAction_Selected()
+        {
+            Console.WriteLine("Version: 23.1.4.8859");
+        }
+
+        private static void timeAction_Selected()
+        {
+            string timeMessage = string.Format("The Current Time Is:{0}{1}{0}", Environment.NewLine, DateTime.Now.ToString("hh:mm:ss"));
+            Console.WriteLine(timeMessage);
+        }
+
+        private static void dateAction_Selected()
+        {
+            string dateMessage = string.Format("The Current Date Is:{0}{1}{0}", Environment.NewLine, DateTime.Now.ToString("dd-MM-yyyy"));
+            Console.WriteLine(dateMessage);
         }
     }
 }
---
+++ Ex04.Menus.Test/VersionPresentation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Test
+{
+    internal class VersionPresentation
+    {
+        public static void OnSelect()
+        {
+            Console.WriteLine("Version: 23.1.4.8859");
+        }
+    }
+}
